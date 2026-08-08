55a1f60a5a90ff00ddf28a64af17c50819774fe9
jishanmujawar33 <jishanmujawar998@gmail.com>
2026-08-08 23:17:42 +0530

    Initial commit

--- ZewelStudio/src/app/data/team.ts
+++ ZewelStudio/src/app/data/team.ts
@@ -4,8 +4,8 @@ import team3 from "../../imports/team-3.jpg";
 import team4 from "../../imports/team-4.jpg";
 
 export const TEAM_MEMBERS = [
-  { name: "Monika Damani", role: "Founder", img: team4 },
-  { name: "CA Rajkumar Damani", role: "Director", img: team3 },
-  { name: "Akash Dhami", role: "Design Head", img: team2, imgClass: "object-[50%_20%]" },
-  { name: "Avani Dhami", role: "Creative Director", img: team1 }
+  { name: "Monika Damani", img: team4 },
+  { name: "CA Rajkumar Damani", img: team3 },
+  { name: "Akash Dhami", img: team2, imgClass: "object-[50%_20%]" },
+  { name: "Avani Dhami", img: team1 }
 ];
